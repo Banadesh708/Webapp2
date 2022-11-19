8b0dc96ba162ecc7b06404a256d145eb84910d18
Banadesh <bannusbali@gmail.com>
2022-11-19 11:29:21 +0530

    added a new class

---
+++ src/main/java/com/example/SpringDemo2/testclass2.java
@@ -0,0 +1,11 @@
+package com.example.SpringDemo2;
+
+public class testclass2 {
+    int a =10;
+    int b=5;
+    int c=a+b;
+    public static void main(String[] args) {
+        testclass2 a= new testclass2();
+        System.out.println(a.c);
+    }
+}
