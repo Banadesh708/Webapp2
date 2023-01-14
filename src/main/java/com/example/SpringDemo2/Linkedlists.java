a520e164f5c8e01bed0025cc9c35d4fb8bbab39b
Banadesh <bannusbali@gmail.com>
2023-01-15 00:59:28 +0530

    added linked lists class

---
+++ src/main/java/com/example/SpringDemo2/Linkedlists.java
@@ -0,0 +1,17 @@
+package com.example.SpringDemo2;
+//a linked list consists of nodes where each node contains a data field and a reference(link) to the next node in the list
+//every element is a separate object with a data part and address part
+//The elements are linked using pointers and addresses. Each element is known as a node
+//The main difference between a normal linked list and a doubly LinkedList is that a doubly linked list contains an extra pointer, typically called the previous pointer, together with the next pointer and data which are there in the singly linked list.
+
+import java.util.LinkedList;
+
+public class Linkedlists {
+    public static void main(String[] args) {
+        LinkedList<String> L1 =new LinkedList<String>();
+        L1.add("Banadesh Bali");
+        L1.add("Btech");
+        L1.add("ECE");
+        System.out.println(L1);
+    }
+}
