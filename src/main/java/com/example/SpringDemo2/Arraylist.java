88304f7c250089eff22f0caac4bf6e3f00e5fe74
Banadesh <bannusbali@gmail.com>
2023-01-15 00:08:53 +0530

    added new classes

---
+++ src/main/java/com/example/SpringDemo2/Arraylist.java
@@ -0,0 +1,19 @@
+package com.example.SpringDemo2;
+import java.util.ArrayList;
+//Array list is an resizable list
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> arr_list= new ArrayList<>();
+        arr_list.add("10");
+        arr_list.add("20");
+        arr_list.add("30");
+        arr_list.add("40");
+        arr_list.add("50");
+        System.out.println(arr_list);
+    }
+
+    private void add(String s) {
+    }
+
+}
